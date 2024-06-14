80cda7b04e05f1feaa5a72d7392d6e600b1e92ad
JKOfficialrepository <150002044+JKOfficialrepository@users.noreply.github.com>
2024-06-14 15:01:26 +0800

    update: package.json

--- next.config.js
+++ next.config.js
@@ -5,10 +5,7 @@ const {withContentlayer} = require("next-contentlayer")
 const nextConfig = {
     compiler:{
         removeConsole: true,
-    },
-    basePath: "/nextjs-tailwindcss-blog",
-    output: "export",
-    reactStrictMode: true,
+    }
 };
 
 module.exports = withContentlayer({ ...nextConfig });
